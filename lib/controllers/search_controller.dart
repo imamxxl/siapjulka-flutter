289dd7cd098af052ee88b57dd282f0889e690d56
ahmadimam <ahmadimamtnt@gmail.com>
2021-11-30 17:46:11 +0700

    update script
    update script

---
+++ lib/controllers/search_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+import 'package:siapjulka/controllers/base_controller.dart';
+
+class SearchController extends GetxController with BaseController {
+  Map<String, dynamic>? body;
+}
